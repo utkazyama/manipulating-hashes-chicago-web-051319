346a1789ae819847c70407dc50d71db2350d53a5
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-24 01:59:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
           if flavor == "strawberry"
-            
+            binding.pry
           end
         end
       end
