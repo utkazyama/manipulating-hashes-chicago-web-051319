ef7d50567360560e5e2b3bd8e540678fda539ce2
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-24 02:04:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,7 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"].each do |person,data|
-    data.each do |attribute,value|
+  contacts["Freddy Mercury"].each do |attribution,value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
           if flavor == "strawberry"
